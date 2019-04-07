8c27b14c3064923069b5fb4289d2181c33663266
zoujianxin <503150692@qq.com>
2019-04-07 17:17:46 +0000

    退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,4 +31,11 @@ class SessionsController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
 }
