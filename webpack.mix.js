7e08f385a5485ae3bc319e893c434ae091ea06b3
zoujianxin <503150692@qq.com>
2019-04-07 13:36:01 +0000

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,3 @@
-const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +10,7 @@ const mix = require('laravel-mix');
  |
  */
 
+const mix = require('laravel-mix');
+
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
