87f4ec7514b3750ef060c1dea8d952df475d66a5
zoujianxin <503150692@qq.com>
2019-04-08 04:29:32 +0000

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
 
     public function gravatar($size = '100')
     {
