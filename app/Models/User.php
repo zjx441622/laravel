2b15c2cc044fb3178e9752b55ab1586606ca2867
zoujianxin <503150692@qq.com>
2019-04-08 05:59:42 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,12 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
